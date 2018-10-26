9fb7f6991f66fb4544ab1d35cb61b9a91f32693c
lei0904 <964358140@qq.com>
2018-10-26 13:47:58 +0800

    spa-web init

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,6 +1,6 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
-var px2rem = require('postcss-px2rem-exclude');
+let px2rem = require('postcss-px2rem-exclude');
 
 module.exports = {
   "plugins": [px2rem({remUnit: 75, exclude: '/node_modules/'})]
